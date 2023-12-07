8b1af5cd2907ca7fffe5b0ac3f7479e8f7d081d6
Davih Duque <davihduque8@gmail.com>
2023-12-07 11:41:37 -0300

    adicionando nova imagem de completar cadastro

--- src/scripts/Data.js
+++ src/scripts/Data.js
@@ -11,3 +11,9 @@ headerRightElement.innerHTML = `
     <h2> Olá, <b style="color: #2864AE;">${usuarioLogado.nome}</b> </h2>
     <h2> Seja bem-vindo(a)</h2>
 `;
+
+// Para pegar o botao de fechar e função para fechar
+const closeApp = document.getElementById("closeApp");
+closeApp.addEventListener("click", function() {
+    fecharEdicao();
+});
